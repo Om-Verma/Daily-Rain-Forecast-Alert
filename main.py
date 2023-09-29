c5e10e186cc83d369d858d110e27934feb9389a2
Om-Verma <46829009+Om-Verma@users.noreply.github.com>
2023-09-30 04:28:35 +0530

    Update main.py
    comments were added.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ UFO = "https://api.openweathermap.org/data/2.5/forecast"
 key = os.environ.get("OWM_API_KEY")
 
 weather_parameters = {
-    "lat": 11.2671,
+    "lat": 11.2671,                       #these are the coordinates for NTI Calicut
     "lon": 75.817,
     "appid": key,
     # "exclude": "current,minutely,daily,alerts"
@@ -16,9 +16,9 @@ response = requests.get(url=UFO, params=weather_parameters)
 response.raise_for_status()
 weather_data=response.json()
 
-will_rain=False         #stores if it is forecasted to rain in the next 12 hrs (3hr period taken)
-for i in range(4):
-    #print(weather_data["list"][i]["weather"][0]["id"])      # i=1 gives forecast after 3 hours. i=2 after 6 hours and so on
+will_rain=False                         #stores if it is forecasted to rain in the next 12 hrs (3hr period taken)
+for i in range(4):                      # i=1 gives forecast after 3 hours. i=2 after 6 hours and so on
+    #print(weather_data["list"][i]["weather"][0]["id"])
     if(weather_data["list"][i]["weather"][0]["id"]<700):
         will_rain=True
 
@@ -34,7 +34,7 @@ from twilio.http.http_client import TwilioHttpClient
 account_sid = os.environ.get("twilio_acc_SID")
 auth_token = os.environ.get("twilio_auth_token")
 
-proxy_client = TwilioHttpClient()
+proxy_client = TwilioHttpClient()                #proxy client was made so that we can access external internet thru python anywhere
 proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
 client = Client(account_sid, auth_token,http_client=proxy_client)
